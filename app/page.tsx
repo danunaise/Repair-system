2f1b17b7a32f975d125d91fcf357d648399254ee
danunnaise <danunai1993@gmail.com>
2023-10-10 00:16:13 +0700

    add loginpage

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,25 @@ import Sidebar from "@/app/components/Sidebar";
 
 export default function Home() {
   return (
-      <div className="flex">
-        <Sidebar />
-        <main className="flex-grow py-4">
-          <h1 className="text-2xl font-bold border-1 border-b pb-2 pl-4">หน้าแรก</h1>
-          {/* เนื้อหาหน้าแรก */}
-        </main>
+      <div className="flex h-screen">
+          <main className="flex-grow flex items-center justify-center">
+              <div>
+                  <div className="flex items-center">
+                      <div className="card bg-white w-[350px] px-5 shadow-lg rounded-md">
+                          <form action="">
+                              <div className="flex-col space-y-2 pb-3">
+                                  <h1 className="text-center text-2xl">เข้าสู่ระบบ</h1>
+                                  <label htmlFor="username" className="text-lg">Username</label>
+                                  <input id="username" type="text" className="block border py-1 w-full rounded-md" />
+                                  <label htmlFor="password" className="text-lg">password</label>
+                                  <input id="password" type="text" className="block border py-1 w-full rounded-md" />
+                                  <button className="bg-blue-400 text-white rounded-md w-full py-1.5 mt-3">เข้าสู่ระบบ</button>
+                              </div>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+          </main>
       </div>
   )
 }
