c4b1da57f741155c669bfd60ff7c5a6e84fc1a65
danunnaise <danunai1993@gmail.com>
2023-10-10 23:43:57 +0700

    addUserpage

--- app/(admin)/addUser/page.tsx
+++ app/(admin)/addUser/page.tsx
@@ -1,17 +1,116 @@
-import React from 'react';
+"use client";
+import React, {Fragment, useState} from 'react';
 import Sidebar from "@/app/components/sidebar_admin/Sidebar";
+import {Listbox, Transition} from "@headlessui/react";
+import {CheckIcon, ChevronUpDownIcon} from "@heroicons/react/20/solid";
 
-
+const people = [
+    { name: 'เลือกแผนก' },
+    { name: 'แผนกการตลาด' },
+    { name: 'แผนกขาย' },
+    { name: 'แผนกบุคคล' },
+    { name: 'แผนกบัญชี/การเงิน' },
+    { name: 'แผนกไอที' },
+]
 const addUserPage = () => {
+    const [selected, setSelected] = useState(people[0])
     return (
-        <div className="flex">
+        <div className="flex h-screen">
             <Sidebar />
-            <main className="flex-grow">
-                <div className="flex items-center pl-20 border-1 border-b py-3">
+            <main className="w-full flex-grow">
+                <div className="flex items-center justify-center border-1 border-b py-3">
                     <svg className="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 8h6m-3 3V5m-6-.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0ZM5 11h3a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z"/>
                     </svg>
                     <span className="text-2xl font-bold ml-3 my-2">เพิ่มสมาชิก</span>
+                </div>
+                <div className="flex items-center justify-center w-full mt-10 py-3">
+                    <div className="card bg-white flex justify-center w-[800px] px-5 shadow-lg rounded-md">
+                    <form className="w-full max-w-lg">
+                        <div className="flex flex-wrap -mx-3 mb-6">
+                            <div className="w-full px-3">
+                                <label htmlFor={"fname"} className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">ชื่อ</label>
+                                <input id={"fname"} name={"fname"}
+                                       type={"text"} placeholder={"ชื่อ"} required
+                                       className="appearance-none block w-full bg-gray-200 text-gray-700 border
+                                       border-gray-200 rounded py-2 px-3 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                />
+                                <label htmlFor={"lname"} className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">นามสกุล</label>
+                                <input id={"lname"} name={"lname"}
+                                       type={"text"} placeholder={"นามสกุล"} required
+                                       className="appearance-none block w-full bg-gray-200 text-gray-700 border
+                                       border-gray-200 rounded py-2 px-3 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                />
+                                <Listbox value={selected} onChange={setSelected}>
+                                    <Listbox.Label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">แผนก</Listbox.Label>
+                                    <div id={"department"} className="flex-1 relative mt-1 block">
+                                        <Listbox.Button className="relative w-full cursor-default rounded bg-white py-1.5 pl-3 pr-10 text-left border-2 focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                                            <span className="block truncate">{selected.name}</span>
+                                            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                                                <ChevronUpDownIcon
+                                                    className="h-5 w-5 text-gray-400"
+                                                    aria-hidden="true"
+                                                />
+                                            </span>
+                                        </Listbox.Button>
+                                        <Transition
+                                            as={Fragment}
+                                            leave="transition ease-in duration-100"
+                                            leaveFrom="opacity-100"
+                                            leaveTo="opacity-0"
+                                        >
+                                            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                                {people.map((person, personIdx) => (
+                                                    <Listbox.Option
+                                                        key={personIdx}
+                                                        className={({ active }) =>
+                                                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                                                active ? 'bg-slate-100 text-slate-900' : 'text-gray-900'
+                                                            }`
+                                                        }
+                                                        value={person}
+                                                    >
+                                                        {({ selected }) => (
+                                                            <>
+                                                                <span
+                                                                    className={`block truncate ${
+                                                                        selected ? 'font-medium' : 'font-normal'
+                                                                    }`}
+                                                                >
+                                                                    {person.name}
+                                                                </span>
+                                                                { selected ? (
+                                                                    <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-blue-600">
+                                                                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                                    </span>
+                                                                ) : null}
+                                                            </>
+                                                        )}
+                                                    </Listbox.Option>
+                                                ))}
+                                            </Listbox.Options>
+                                        </Transition>
+                                    </div>
+                                </Listbox>
+                                <label htmlFor={"Username"} className="block uppercase tracking-wide text-gray-700 mt-3 text-xs font-bold mb-2">Username</label>
+                                <input id={"Username"} name={"Username"}
+                                       type={"text"} placeholder={"Username"} required
+                                       className="appearance-none block w-full bg-gray-200 text-gray-700 border
+                                       border-gray-200 rounded py-2 px-3 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                />
+                                <label htmlFor={"password"} className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">password</label>
+                                <input id={"password"} name={"password"}
+                                       type={"text"} placeholder={"password"} required
+                                       className="appearance-none block w-full bg-gray-200 text-gray-700 border
+                                       border-gray-200 rounded py-2 px-3 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                />
+                                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                                    บันทึก
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                    </div>
                 </div>
             </main>
         </div>
--- app/components/sidebar_admin/Sidebar.tsx
+++ app/components/sidebar_admin/Sidebar.tsx
@@ -9,33 +9,40 @@ const Sidebar = (props: Props) => {
     const Navlink = [
         {
             name: "หน้าแรก",
-            path: "/",
+            path: "/dashboard",
             icon: <svg className="w-4 h-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" d="M3 8v10a1 1 0 0 0 1 1h4v-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v5h4a1 1 0 0 0 1-1V8M1 10l9-9 9 9"/>
             </svg>
         },
         {
             name: "เพิ่มสมาชิก",
-            path: "/report",
+            path: "/addUser",
+            icon: <svg className="w-4 h-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M13 8h6m-3 3V5m-6-.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0ZM5 11h3a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z"/>
+            </svg>
+        },
+        {
+            name: "แก้ไขสมาชิก",
+            path: "/editUser",
             icon: <svg className="w-4 h-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 21">
                 <path stroke="currentColor" d="M7.418 17.861 1 20l2.139-6.418m4.279 4.279 10.7-10.7a3.027 3.027 0 0 0-2.14-5.165c-.802 0-1.571.319-2.139.886l-10.7 10.7m4.279 4.279-4.279-4.279m2.139 2.14 7.844-7.844m-1.426-2.853 4.279 4.279"/>
             </svg>
         },
         {
-            name: "แก้ไขสมาชิก",
-            path: "/trace",
-            icon: <svg className="w-4 h-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="none" viewBox="0 0 18 20">
-                <path stroke="currentColor" d="M12 2h4a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h4m6 0v3H6V2m6 0a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1M5 5h8m-5 5h5m-8 0h.01M5 14h.01M8 14h5"/>
-            </svg>
-        },
-        {
             name: "แก้ไขข้อมูลทั่วไป",
-            path: "/trace",
+            path: "/edit",
             icon: <svg className="w-4 h-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <g stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1">
                     <path d="M19 11V9a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L12 2.757V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L2.929 4.343a1 1 0 0 0 0 1.414l.536.536L2.757 8H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535L8 17.243V18a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H18a1 1 0 0 0 1-1Z"/>
                     <path d="M10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
                 </g>
+            </svg>
+        },
+        {
+            name: "จัดการรายการปัญหา",
+            path: "/trace",
+            icon: <svg className="w-4 h-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="none" viewBox="0 0 18 20">
+                <path stroke="currentColor" d="M12 2h4a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h4m6 0v3H6V2m6 0a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1M5 5h8m-5 5h5m-8 0h.01M5 14h.01M8 14h5"/>
             </svg>
         },
         {
